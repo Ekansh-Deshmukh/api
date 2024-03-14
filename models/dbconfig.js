1a566e47608b15839e7bf89d01d152ab9255ab1f
Ekansh Deshmukh <ekanshdeshmukh290@gmail.com>
2024-03-14 16:33:02 +0530

    my fourth commit

--- models/dbconfig.js
+++ models/dbconfig.js
@@ -2,8 +2,8 @@ const mongoose =  require('mongoose');
 
 exports.dbconnection = async ()=>{
     try{
-       mongoose.connect(process.env.MONGO_URL)
-        
+       mongoose.connect(process.env.MONGO_URL) 
+       
         console.log('DB Connected');
     }catch(err){
         console.log(err.message);
