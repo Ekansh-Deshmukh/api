b3c4949aa4ce43a7e93c9edac9557fdb115b6aec
Ekansh Deshmukh <ekanshdeshmukh290@gmail.com>
2024-03-15 21:10:29 +0530

    Added userSchema and controllers in api

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-require('dotenv').config({path:"./.env"});
-
-const express = require('express');
+require("dotenv").config({ path: "./.env" });
+const express = require("express");
 const app = express();
 
-require('./models/dbconfig.js').dbconnection();
+// db connection
+require("./models/dbconfig").dbconnection();
 
-app.listen( process.env.PORT, function(){
-    console.log(`server is running on PORT ${process.env.PORT}`);
-})
+// routes
+const userRouter = require("./routes/userRoute");
+
+// logger
+const logger = require("morgan");
+app.use(logger("tiny"));
+// body pasrser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/user", userRouter);
+
+app.all("*", (req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: `${req.url} route not found`,
+    });
+});
+
+// server
+app.listen(process.env.PORT, () => {
+    console.log(`The Server is running on port ${process.env.PORT}`);
+});
--- models/dbconfig.js
+++ models/dbconfig.js
@@ -1,11 +1,10 @@
-const mongoose =  require('mongoose');
+const mongoose = require("mongoose");
 
-exports.dbconnection = async ()=>{
-    try{
-       mongoose.connect(process.env.MONGO_URL) 
-       
-        console.log('DB Connected');
-    }catch(err){
-        console.log(err.message);
+exports.dbconnection = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URL);
+        console.log("Connection Established!");
+    } catch (error) {
+        console.log(error.message);
     }
-}
+};
