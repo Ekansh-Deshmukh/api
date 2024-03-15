9499912ac1580f984129f4d8aa920dfa2ce0e6cc
Ekansh Deshmukh <ekanshdeshmukh290@gmail.com>
2024-03-15 21:09:53 +0530

    Added userSchema and controllers

--- app.js
+++ app.js
@@ -5,19 +5,6 @@ const app = express();
 
 require('./models/dbconfig.js').dbconnection();
 
-//routes
-const userRouter = require('./routes/userRoute.js');
-
-
-//body parser
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-
-app.use('/api/user',userRouter);
-
-
-//server
 app.listen( process.env.PORT, function(){
     console.log(`server is running on PORT ${process.env.PORT}`);
 })
